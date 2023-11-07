44a83003476cdd66eb20523a87db27540717fd1c
s23001 <s23001@std.it-college.ac.jp>
2023-11-07 16:57:59 +0900

    chapter6-2まで終わった

---
+++ src/chapter6/Tax.java
@@ -0,0 +1,16 @@
+package chapter6;
+
+public class Tax {
+    private int number;
+    private String name;
+    private int shotoku;
+    private int koujo;
+
+    public Tax(int i, String 田中宏, int i1, int i2) {
+    }
+
+    public int zeigaku() {
+        return (int) ((shotoku - koujo) * 0.1);
+       }
+    }
+
